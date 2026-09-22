2373fbba97ee8db071dfd8fac062751be40b2c41
Fangyijing Huang <fh2419@nyu.edu>
2026-09-22 15:30:14 -0400

    all new changes

---
+++ class-demos/Class5/main.js
@@ -0,0 +1,16 @@
+// this is a comment
+// syntax is //
+
+alert('javascript!')
+
+console.log('log this info to the console.')
+
+// window.addEventListener("load", ()=>{
+//     console.log("fully loaded");
+// })
+
+// short hand for waiting for the webpage to load
+
+window.onload=() => {
+    console.log("page has loaded.")
+}
